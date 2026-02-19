658ad5956cfaf6a0affbdecfbe619f309e9bd852
Lin kouk <menglinboth@gmail.com>
2026-02-19 15:47:00 +0700

    add

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Fortify\CreateNewUser;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function Register(Request $request, CreateNewUser $create)
+    {
+        $user = $create->create($request->all());
+
+        event(new Registered($user));
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User registered successfully,verify your email',
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function Login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+        if (!$user->hasVerifiedEmail()){
+            return response()->json([
+                'message' => 'Please verify your email address before logging in.'
+            ], 403);
+        }
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'Login successful',
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register',[AuthController::class,'Register']);
+Route::post('/login', [AuthController::class,'Login']);
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill(); 
+    return response()->json([
+        'message' => 'Email verified successfully'
+    ]);
+})->middleware(['auth:sanctum']) // Protect route
+  ->name('verification.verify'); 
